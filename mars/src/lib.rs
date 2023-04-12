291ecb4031135e8e3b77c9b1b12607655a58cdc5
m. interrupt <misterinterrupt@gmail.com>
2023-04-11 23:55:24 -0700

    starting MARS lib

--- mars/src/lib.rs
+++ mars/src/lib.rs
@@ -1,3 +1,41 @@
-pub fn thing() {
-    println!("Hello, world!");
+pub struct Store<Action, State> {
+    dispatch: std::sync::mpsc::Sender<Action>,
+    state: State,
+}
+
+impl<Action, State> Store<Action, State> {
+    pub fn new(state: State) -> Store<Action, State> {
+        let (tx, _rx) = std::sync::mpsc::channel::<Action>();
+        Store {
+            dispatch: tx,
+            state,
+        }
+    }
+    pub fn update(&self, action: Action) {
+        self.dispatch.send(action).unwrap();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn can_create_store() {
+        enum Action {}
+        let s = Vec::<usize>::new();
+        let store = Some(Store::<Action, Vec<usize>>::new(s));
+        assert!(store.is_some());
+    }
+
+    #[test]
+    fn can_dispatch_same_thread() {
+        assert_eq!(false, true);
+    }
+
+    #[test]
+    fn can_dispatch_different_thread() {
+        assert_eq!(false, true);
+    }
 }
