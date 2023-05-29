4bbb2ba4671de92a4a460a6235b568486f4abf2a
m. interrupt <misterinterrupt@gmail.com>
2023-05-29 15:35:49 -0700

    exercise to understand using basedrop crate

--- droptest/src/main.rs
+++ droptest/src/main.rs
@@ -1,20 +1,59 @@
 extern crate basedrop;
 
-use basedrop::{Collector, Owned, Shared};
+use basedrop::{Collector, Shared};
 fn main() {
     println!("running basedrop test");
-    println!("making a Collector");
-    let collector = Collector::new();
-    println!("making an Owned value");
-    let three: Owned<i32> = Owned::new(&collector.handle(), 3);
-    println!("making a Shared value");
-    let sha: Shared<&str> = Shared::new(&collector.handle(), "sha");
-    println!("making a thread");
-    let handle_1 = std::thread::spawn(move || {
+    let (tx_send, rx_send) = std::sync::mpsc::channel::<Shared<i32>>();
+    let (tx_return, rx_return) = std::sync::mpsc::channel::<Shared<i32>>();
+    let collector_handle = std::thread::spawn(move || {
+        let max = 100;
+        let mut current = 0;
+        let mut collector = Collector::new();
+        let data = current;
+        let state = Shared::new(&collector.handle(), data);
         loop {
-            println!("Shared value in thread loop: {}", sha.chars().as_str());
+            match tx_send.send(state.clone()) {
+                Ok(_) => {
+                    println!("ptr #{} sent from collector thread", current);
+                }
+                Err(_) => println!("failed to send ptr from collector thread"),
+            }
+            rx_return.try_iter().for_each(|p| {
+                current += 1;
+                println!("ptr #{} returned to collector thread", p.to_string());
+            });
+            if current >= max {
+                println!("all shared ptrs returned to collector thread");
+                break;
+            }
+            collector.collect();
         }
     });
+    let user_handle = std::thread::spawn(move || {
+        let mut count = 0;
+        loop {
+            let res: Result<Shared<i32>, std::sync::mpsc::TryRecvError> =
+                rx_send.try_recv().to_owned();
+            if res.is_ok() {
+                let ptr = res.unwrap();
+                println!("ptr #{} recieved in user thread", ptr.to_string());
+                match tx_return.send(ptr) {
+                    Ok(_) => {
+                        count += 1;
+                        println!("user thread returned ptr #{}", count);
+                        if count >= 100 {
+                            break;
+                        }
+                    }
+                    Err(_) => println!("user thread failed to return ptr"),
+                }
+            } else {
+                println!("no ptr received in user thread");
+            }
+        }
+    });
+    collector_handle.join().unwrap();
+    user_handle.join().unwrap();
 }
 
 #[cfg(test)]
