a5d5be5135a1f39922b4a3fc299177d7e29d738c
m. interrupt <misterinterrupt@gmail.com>
2023-05-30 16:21:33 -0700

    remove the return queue

--- droptest/src/main.rs
+++ droptest/src/main.rs
@@ -4,12 +4,10 @@ use basedrop::{Collector, Shared};
 fn main() {
     println!("running basedrop test");
     let (tx_send, rx_send) = std::sync::mpsc::channel::<Shared<i32>>();
-    let (tx_return, rx_return) = std::sync::mpsc::channel::<Shared<i32>>();
     let max = 100;
     let collector_handle = std::thread::spawn(move || {
-        let mut current = 0;
+        let mut current = 1;
         let mut collector = Collector::new();
-        let mut returned = 0;
         loop {
             let data = current;
             let state = Shared::new(&collector.handle(), data);
@@ -20,38 +18,18 @@ fn main() {
                 }
                 Err(_) => println!("failed to send ptr from collector thread"),
             }
-            rx_return.try_iter().for_each(|p| {
-                returned += 1;
-                println!("{} returned to collector thread", *p);
-            });
-            if returned >= max {
+            if current > max {
                 println!("all shared ptrs returned to collector thread");
-                break;
+                std::process::exit(1);
             }
             collector.collect();
         }
     });
-    let user_handle = std::thread::spawn(move || {
-        let mut count = 0;
-        loop {
-            let res: Result<Shared<i32>, std::sync::mpsc::TryRecvError> =
-                rx_send.try_recv().to_owned();
-            if let Ok(ptr) = res {
-                let data = *ptr;
-                println!("{} recieved in user thread", data);
-                match tx_return.send(ptr) {
-                    Ok(_) => {
-                        count += 1;
-                        println!("user thread returned {}", data);
-                        if count >= max {
-                            break;
-                        }
-                    }
-                    Err(_) => println!("user thread failed to return ptr"),
-                }
-            } else {
-                // println!("no ptr received in user thread");
-            }
+    let user_handle = std::thread::spawn(move || loop {
+        let res: Result<Shared<i32>, std::sync::mpsc::TryRecvError> = rx_send.try_recv().to_owned();
+        if let Ok(ptr) = res {
+            let data = *ptr;
+            println!("{} recieved in user thread", data);
         }
     });
     collector_handle.join().unwrap();
