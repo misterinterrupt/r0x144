77fa5ba55ef5886a5d2bbb0b09c80f321407fe7b
m. interrupt <misterinterrupt@gmail.com>
2023-06-15 20:12:52 -0700

    change store from Arc on two diff threads

--- imtest/src/lib.rs
+++ imtest/src/lib.rs
@@ -7,13 +7,16 @@ mod imtest {
     use im::{HashMap, Vector};
     use serde::{Deserialize, Serialize};
     #[derive(Debug, PartialEq, Eq, Hash, Deserialize, Serialize)]
-    pub struct State<T: Clone> {
+    pub struct State<T>
+    where
+        T: Clone + Send + Sync,
+    {
         history: Vector<HashMap<String, T>>,
         data: HashMap<String, T>,
     }
     impl<T> State<T>
     where
-        T: Clone + PartialEq + Eq,
+        T: Clone + Send + Sync + PartialEq + Eq,
     {
         pub fn new() -> Self {
             Self {
@@ -21,9 +24,9 @@ mod imtest {
                 data: HashMap::new(),
             }
         }
-        pub fn insert(&mut self, key: String, value: T) {
-            self.history.push_back(self.data.clone());
-            self.data.insert(key, value);
+        pub fn insert(&self, key: String, value: T) {
+            self.history.clone().push_back(self.data.clone());
+            self.data.clone().insert(key, value);
         }
         pub fn len(&self) -> usize {
             self.data.len()
@@ -31,11 +34,16 @@ mod imtest {
         pub fn hist_len(&self) -> usize {
             self.history.len()
         }
+        pub fn get(&self, key: &str) -> Option<T> {
+            self.data.get(key).cloned()
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::sync::Arc;
+
     use super::imtest::State;
 
     #[test]
@@ -49,12 +57,49 @@ mod tests {
 
     #[test]
     fn insert_makes_history_after_one() {
-        let mut state = State::new();
+        let state = State::new();
         state.insert("a".to_string(), 1);
         assert_eq!(state.hist_len(), 1);
         state.insert("a".to_string(), 2);
         assert_eq!(state.hist_len(), 2);
         state.insert("a".to_string(), 3);
         assert_eq!(state.hist_len(), 3);
+    }
+
+    #[test]
+    fn send_sync_two_thread_edit() {
+        let state = Arc::new(State::new());
+        state.insert("t1a".to_string(), 1);
+        state.insert("t1b".to_string(), 2);
+        state.insert("t2a".to_string(), 3);
+        state.insert("t2b".to_string(), 4);
+        let s1 = state.clone();
+        let s2 = s1.clone();
+        let s3 = s1.clone();
+        let s4 = s1.clone();
+
+        let (tx1, rx1) = std::sync::mpsc::channel::<Arc::<State<i32>>>();
+
+        let t1 = std::thread::spawn(move || {
+            s1.insert("t1a".to_string(), 5);
+            s3.insert("t1b".to_string(), 6);
+            tx1.send(s1).unwrap();
+        });
+        let t2 = std::thread::spawn(move || {
+            s2.insert("t2a".to_string(), 7);
+            s4.insert("t2b".to_string(), 8);
+            match rx1.try_recv() {
+                Ok(state) => {
+                    assert_eq!(state.get("t1a").unwrap(), 5);
+                    assert_eq!(state.get("t1b").unwrap(), 6);
+                }
+                Err(_) => {}
+            }
+        });
+        (t1.join().unwrap(), t2.join().unwrap());
+        assert_eq!(state.get("t1a").unwrap(), 5);
+        assert_eq!(state.get("t1b").unwrap(), 6);
+        assert_eq!(state.get("t2a").unwrap(), 7);
+        assert_eq!(state.get("t2b").unwrap(), 8);
     }
 }
